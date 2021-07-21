f3f996df113b293dd8b4fc32139bb47bf2cc525d
Mona Abdel-Keream <abdelker@uni-bremen.de>
2021-07-21 09:29:05 +0200

    updated logging package

--- cram_knowrob/cram_cloud_logger/src/neem-interface.pl
+++ cram_knowrob/cram_cloud_logger/src/neem-interface.pl
@@ -2,16 +2,25 @@
 :- use_module(library('db/mongo/client')).
 :- dynamic execution_agent/1.
 
-mem_episode_start(Action) :- retractall(execution_agent(_)),tf_logger_disable, tripledb_drop(),forall(mng_collection(roslog,Coll),mng_drop(roslog,Coll)), tf_logger_enable,
+mem_episode_start(Action,
+     EnvOwl,
+EnvOwlIndiName,
+EnvUrdf,
+EnvUrdfPrefix,
+AgentOwl,
+AgentOwlIndiName,
+AgentUrdf) :- 
+    retractall(execution_agent(_)),tf_logger_disable, tripledb_drop(),forall(mng_collection(roslog,Coll),mng_drop(roslog,Coll)), tf_logger_enable,tf_mem_clear,
     tripledb_load('package://knowrob/owl/knowrob.owl',[graph(tbox),namespace(knowrob)]),
-    tripledb_load('package://iai_semantic_maps/owl/kitchen.owl'),
-    tripledb_load('package://iai_avatar_description/Avatars/yasmin/owl/avatar_yasmin.owl'),
-    urdf_load('http://knowrob.org/kb/avatar_yasmin.owl#avatar_yasmin_robot1', 'package://iai_avatar_description/Avatars/yasmin/urdf/avatar_yasmin.urdf', [load_rdf]),
-    urdf_load('http://knowrob.org/kb/IAI-kitchen.owl#iai_kitchen_room_link', 'package://iai_kitchen/urdf_obj/iai_kitchen_python.urdf.xarco', [load_rdf,prefix('iai_kitchen/')]),
-    assertz(execution_agent('http://knowrob.org/kb/avatar_yasmin.owl#avatar_yasmin_robot1')),
+    tripledb_load(EnvOwl),
+    tripledb_load(AgentOwl),
+    urdf_load(AgentOwlIndiName, AgentUrdf, [load_rdf]),
+    urdf_load(EnvOwlIndiName, EnvUrdf, [load_rdf,prefix(EnvUrdfPrefix)]),
+    assertz(execution_agent(AgentOwlIndiName)),
     execution_agent(Agent),
     tell([is_episode(Episode), is_action(Action), has_type(Task,soma:'PhysicalTask'), 
-            executes_task(Action,Task),is_setting_for(Episode,Action), is_performed_by(Action,Agent)]),notify_synchronize(event(Action)),!.
+            executes_task(Action,Task),is_setting_for(Episode,Action), is_performed_by(Action,Agent), has_type(Role, soma:'AgentRole'), has_role(Agent,Role)]),notify_synchronize(event(Action)),!.
+    %%executes_task(Action,Task),is_setting_for(Episode,Action),is_setting_for(Episode,Agent)]),notify_synchronize(event(Action)),!.
 
 %is_recording_episode(Result) :- assertz(cramEpisodeName('None')), retract(cramEpisodeName('None')), (cramEpisodeName(Name) -> Result = Name ; Result = 'NoName').
 %delete_episode_name(Name) :- retract(cramEpisodeName(Name)).
@@ -20,15 +29,27 @@ mem_event_set_failed(Action) :- tell(action_failed(Action)).
 mem_event_set_succeeded(Action) :- tell(action_succeeded(Action)).
 mem_event_add_diagnosis(Situation, Diagnosis) :- tell(satisfies(Situation, Diagnosis)).
 
-add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent), tell([is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction),is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
+%add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent), tell([has_type(Role, soma:'AgentRole'), has_role(Agent,Role), is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction),is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
 
-mem_event_end(Event) :- get_time(CurrentTime),ask(triple(Event,dul:'hasTimeInterval',TimeInterval)),tripledb_forget(TimeInterval, soma:'hasIntervalEnd', _),tell(holds(TimeInterval, soma:'hasIntervalEnd', CurrentTime)),!.
+add_subaction_with_task(Action,SubAction,TaskType) :- execution_agent(Agent),tell([is_action(SubAction), has_type(Task,TaskType), executes_task(SubAction,Task), has_subevent(Action,SubAction), is_performed_by(SubAction,Agent)]),notify_synchronize(event(Event)), !.
+
+mem_event_end(Event) :- execution_agent(Agent),get_time(CurrentTime), ask([triple(Event,dul:'hasTimeInterval',TimeInterval), triple(TimeInterval,soma:'hasIntervalBegin', Start), executes_task(Event,Task)]),tripledb_forget(TimeInterval, soma:'hasIntervalEnd', _),tell([holds(TimeInterval, soma:'hasIntervalEnd', CurrentTime),has_type(Role, soma:'AgentRole'), has_role(Agent,Role) during Event,task_role(Task, Role)]),!.
 
 mem_event_begin(Event) :- get_time(CurrentTime),tell(occurs(Event) since CurrentTime),!.
 
-%belief_perceived_at(ObjectType, Frame, Object) :- get_time(CurrentTime),tell([has_type(Object,ObjectType),is_at(Object,Frame) since CurrentTime]).
-belief_perceived_at(ObjectType, Frame, Object) :- tell([has_type(Object,ObjectType)]).
-add_participant_with_role(Action, ObjectId, RoleType) :- tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0]]).
+%belief_perceived_at(ObjectType, Frame, Object) :- get_time(CurrentTime),execution_agent(Agent),tell([has_type(Object,ObjectType),is_at(Object,Frame) since CurrentTime]).
+
+belief_perceived_at(ObjectType, Mesh, Rotation, Object) :- tell([has_type(Object,ObjectType),has_type(ShapeRegion, soma:'MeshShape'), has_type(Shape, soma:'Shape'), triple(Object, soma:'hasShape', Shape), triple(Shape, dul:'hasRegion', ShapeRegion), triple(ShapeRegion, soma:'hasFilePath', Mesh),has_type(Origin,soma:'Origin'),triple(ShapeRegion,'http://knowrob.org/kb/urdf.owl#hasOrigin',Origin),triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasPositionVector', term([0.0,0.0,0.0])),triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasOrientationVector',term(Rotation))]).
+
+belief_perceived_at(ObjectType, Object) :- tell([has_type(Object,ObjectType)]).
+
+
+add_participant_with_role(Action, ObjectId, RoleType) :- 
+ask([executes_task(Action, Task),triple(Event,dul:'hasTimeInterval',TimeInterval),triple(TimeInterval,soma:'hasIntervalBegin',Start),triple(TimeInterval,soma:'hasIntervalEnd',End)]), 
+tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during Action,task_role(Task, Role)]).
+
+%add_participant_with_role(Action, ObjectId, RoleType) :- ask(executes_task(Action, Task)), tell([has_participant(Action,ObjectId), has_type(Role, RoleType), has_role(ObjectId,Role) during [0.0,0.0]]).
+
 add_parameter(Task,ParameterType,RegionType) :- tell([has_type(Parameter, ParameterType), has_type(Region,RegionType),has_assignment(Parameter,Region) during [0.0,0.1], has_parameter(Task, Parameter)]).
 
 add_grasping_parameter(Action,GraspingOrientationType) :- ask(executes_task(Action, Task)), tell([has_type(GraspingOrientation,GraspingOrientationType), has_type(GraspingOrientationConcept,'http://www.ease-crc.org/ont/SOMA.owl#GraspingOrientation'), has_parameter(Task,GraspingOrientationConcept),holds(GraspingOrientationConcept, dul:classifies, GraspingOrientation),has_region(Action,GraspingOrientation)]),!.
